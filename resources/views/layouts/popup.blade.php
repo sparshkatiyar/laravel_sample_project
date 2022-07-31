5d7ac48cd59b92445b6b22774cc06ea4dd2e18f3
Vikash Chauhan <vikash@ogrelogic.com>
2022-07-31 22:39:10 +0530

    otp design update

--- resources/views/layouts/popup.blade.php
+++ resources/views/layouts/popup.blade.php
@@ -44,7 +44,7 @@
         <p class="otpheading">OTP sent to <span> +91 1234567890</span></p>
       </div>
       <form action="" class="mt-5">
-        <input
+        <!-- <input
           class="otp"
           type="text"
           oninput="digitValidate(this)"
@@ -71,7 +71,15 @@
           oninput="digitValidate(this)"
           onkeyup="tabChange(4)"
           maxlength="1"
-        />
+        /> -->
+		<div class="otp-field">
+		<input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1"  value="" inputtype="numeric" id="otc-2" required>
+		<input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1"  value="" inputtype="numeric" id="otc-3" required>
+		<input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1"  value="" inputtype="numeric" id="otc-4" required>
+		<input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1"  value="" inputtype="numeric" id="otc-5" required>
+		
+		</div>
+	
       </form>
       <button class="main-button-index" id="otp-submit" onclick="submitotp()">Submit</button>
 
@@ -92,3 +100,61 @@
   </div>
 
     <!-- -------popup end------- -->
+
+    <script>
+     let in1 = document.getElementById('otc-1'),
+	ins = document.querySelectorAll('input[type="number"]'),
+	splitNumber = function(e) {
+		let data = e.data || e.target.value;
+		if (!data) return;
+		if (data.length === 1) return;
+
+		popuNext(e.target, data);
+		//for (i = 0; i < data.length; i++ ) { ins[i].value = data[i]; }
+	},
+	popuNext = function(el, data) {
+		el.value = data[0];
+		data = data.substring(1);
+		if (el.nextElementSibling && data.length) {
+
+			popuNext(el.nextElementSibling, data);
+		}
+	};
+
+ins.forEach(function(input) {
+
+	input.addEventListener('keyup', function(e) {
+
+		if (e.keyCode === 16 || e.keyCode == 9 || e.keyCode == 224 || e.keyCode == 18 || e.keyCode == 17) {
+			return;
+		}
+
+		if ((e.keyCode === 8 || e.keyCode === 37) && this.previousElementSibling && this.previousElementSibling.tagName === "INPUT") {
+			this.previousElementSibling.select();
+		} else if (e.keyCode !== 8 && this.nextElementSibling) {
+			this.nextElementSibling.select();
+		}
+
+
+		if (e.target.value.length > 1) {
+			splitNumber(e);
+		}
+	});
+
+	input.addEventListener('focus', function(e) {
+
+		if (this === in1) return;
+
+		if (in1.value == '') {
+			in1.focus();
+		}
+
+		if (this.previousElementSibling.value == '') {
+			this.previousElementSibling.focus();
+		}
+	});
+});
+
+in1.addEventListener('input', splitNumber);
+
+    </script>
