8a6caf55563b429d0a3718bddb55093c31745592
pk prajapati <pappu.kumar@fluper.in>
2022-08-04 12:28:25 +0530

    asd

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -7,9 +7,9 @@
             <div class="contact-div">
                 <div class="item1">
                     <img src="https://img.icons8.com/ios/2x/apple-phone.png" alt="javascript:void(0);" width="20px" />
-                    <p><a href=""> 1234567890</a></p>
+                    <p><a href="tel:+91 88106 40406 "> +91 88106 40406</a></p>
                     <img src="https://img.icons8.com/ios/2x/apple-mail.png" alt="javascript:void(0);" width="25px" />
-                    <p><a href=""> tim.jennings@example.com</a></p>
+                    <p><a href="mailto:info@astropanditom.com">  info@astropanditom.com</a></p>
                 </div>
                 <div class="item2">
                     <img src="https://img.icons8.com/material-outlined/2x/search.png" alt="javascript:void(0);" width="20px" />
