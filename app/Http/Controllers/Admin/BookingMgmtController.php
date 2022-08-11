35ea965688d47682d26c81581a18594552e79b1c
pk prajapati <pappu.kumar@fluper.in>
2022-08-11 19:53:17 +0530

    admin iisue

--- app/Http/Controllers/Admin/BookingMgmtController.php
+++ app/Http/Controllers/Admin/BookingMgmtController.php
@@ -9,18 +9,21 @@ use App\Models\Pandit;
 use App\Models\PujaEcommerce;
 use App\Models\UserAddress;
 use App\Models\Puja;
+use App\Models\User;
 
 
 class BookingMgmtController extends Controller
 {
     public function index(){
         $bookingList = Booking::where('pandit_id',null)->orderBy('id', 'DESC')->paginate(5);
+        // dd($bookingList);
         $panditList = Pandit::all();
         foreach(@$bookingList as $booking){
-            $booking->ecomm_puja_id     = PujaEcommerce::find($booking->ecomm_puja_id);
-            $booking->ecomm_puja_id->puja_id     = Puja::find($booking->ecomm_puja_id->puja_id);            
-            $booking->address_id        = UserAddress::find($booking->address_id);
-            $booking->pandit_id         = Pandit::find($booking->pandit_id);
+            $booking->ecomm_puja_id                 = PujaEcommerce::find($booking->ecomm_puja_id);
+            $booking->ecomm_puja_id->puja_id        = Puja::find($booking->ecomm_puja_id->puja_id);            
+            $booking->address_id                    = UserAddress::find($booking->address_id);
+            $booking->user_details                  = User::find($booking->user_id);
+            $booking->pandit_id                     = Pandit::find($booking->pandit_id);
         } 
         // dd($bookingList);
         return view('admin/booking-list' ,compact('bookingList','panditList'));
