f405d19f8f4717fff06528fce2084380a43ec1a8
pk prajapati <pappu.kumar@fluper.in>
2022-08-13 19:49:51 +0530

    M3 Admin panle issue done

--- resources/views/layouts/popup.blade.php
+++ resources/views/layouts/popup.blade.php
@@ -16,7 +16,7 @@
             <form action="javascript:void(0);" id="registerForm">
 
                 <div class="second-div">
-                    <select class="optiondiv" name="mobileTelCode" id="mobileTelCode">
+                    <select class="optiondiv" name="mobileTelCode" id="mobileTelCode" required>
         
                         
                         <option value="+91">IND</option>
@@ -106,60 +106,60 @@
     <!-- -------popup end------- -->
 
     <script>
-     let in1 = document.getElementById('otc-1'),
-	ins = document.querySelectorAll('input[type="number"]'),
-	splitNumber = function(e) {
-		let data = e.data || e.target.value;
-		if (!data) return;
-		if (data.length === 1) return;
+        let in1 = document.getElementById('otc-1'),
+        ins = document.querySelectorAll('input[type="number"]'),
+        splitNumber = function(e) {
+            let data = e.data || e.target.value;
+            if (!data) return;
+            if (data.length === 1) return;
 
-		popuNext(e.target, data);
-		//for (i = 0; i < data.length; i++ ) { ins[i].value = data[i]; }
-	},
-	popuNext = function(el, data) {
-		el.value = data[0];
-		data = data.substring(1);
-		if (el.nextElementSibling && data.length) {
+            popuNext(e.target, data);
+            //for (i = 0; i < data.length; i++ ) { ins[i].value = data[i]; }
+        },
+        popuNext = function(el, data) {
+            el.value = data[0];
+            data = data.substring(1);
+            if (el.nextElementSibling && data.length) {
 
-			popuNext(el.nextElementSibling, data);
-		}
-	};
+                popuNext(el.nextElementSibling, data);
+            }
+        };
 
-ins.forEach(function(input) {
+        ins.forEach(function(input) {
 
-	input.addEventListener('keyup', function(e) {
+            input.addEventListener('keyup', function(e) {
 
-		if (e.keyCode === 16 || e.keyCode == 9 || e.keyCode == 224 || e.keyCode == 18 || e.keyCode == 17) {
-			return;
-		}
+                if (e.keyCode === 16 || e.keyCode == 9 || e.keyCode == 224 || e.keyCode == 18 || e.keyCode == 17) {
+                    return;
+                }
 
-		if ((e.keyCode === 8 || e.keyCode === 37) && this.previousElementSibling && this.previousElementSibling.tagName === "INPUT") {
-			this.previousElementSibling.select();
-		} else if (e.keyCode !== 8 && this.nextElementSibling) {
-			this.nextElementSibling.select();
-		}
+                if ((e.keyCode === 8 || e.keyCode === 37) && this.previousElementSibling && this.previousElementSibling.tagName === "INPUT") {
+                    this.previousElementSibling.select();
+                } else if (e.keyCode !== 8 && this.nextElementSibling) {
+                    this.nextElementSibling.select();
+                }
 
 
-		if (e.target.value.length > 1) {
-			splitNumber(e);
-		}
-	});
+                if (e.target.value.length > 1) {
+                    splitNumber(e);
+                }
+            });
 
-	input.addEventListener('focus', function(e) {
+            input.addEventListener('focus', function(e) {
 
-		if (this === in1) return;
+                if (this === in1) return;
 
-		if (in1.value == '') {
-			in1.focus();
-		}
+                if (in1.value == '') {
+                    in1.focus();
+                }
 
-		if (this.previousElementSibling.value == '') {
-			this.previousElementSibling.focus();
-		}
-	});
-});
+                if (this.previousElementSibling.value == '') {
+                    this.previousElementSibling.focus();
+                }
+            });
+        });
 
-in1.addEventListener('input', splitNumber);
+        // in1.addEventListener('input', splitNumber);
 
     </script>
 
@@ -202,7 +202,7 @@ in1.addEventListener('input', splitNumber);
                     $('.second').html(minutes + ':' + seconds);
                     timer2 = minutes + ':' + seconds;
                     }, 1000);
-                    continiuepop();
+                    // continiuepop();
                     // alert('Ajax form has been submitted successfully');
                 }
             });
@@ -232,6 +232,5 @@ in1.addEventListener('input', splitNumber);
         });
     </script>
     <script>
-        // var timer2 = "1:01";
-        // var interval = 
+        
     </script>
