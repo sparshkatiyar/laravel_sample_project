eabf79de4fbb4822bb617d9747f58cf77063aaae
Vikash Chauhan <Vikash.chauhan@fluper.in>
2022-09-16 23:46:31 +0530

    text editor

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -4,10 +4,13 @@
         <div class="text-sm text-muted">Version 1.1.1</div>
     </div>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/feather-icons"></script>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://unpkg.com/feather-icons"></>
   <script>
     feather.replace()
   </script>
-<!-- <script src="{{ asset('admin/assets/js/site.min.js')}}"></script> -->
 
+<script src="{{ asset('admin/assets/js/site.min.js')}}"></script>
+<script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
--- resources/views/admin/head.blade.php
+++ resources/views/admin/head.blade.php
@@ -7,7 +7,6 @@
     <meta name="description" content="Responsive, Bootstrap, BS4" />
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
     <link rel="stylesheet" href="{{ asset('admin/assets/css/site.min.css')}}" />
-    <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    
+   
 </head>
